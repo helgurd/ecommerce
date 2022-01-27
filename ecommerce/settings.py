68b7b91bcc8bfed11b01db7f4268a894fe816273
helgurd <rekauta@gmail.com>
2022-01-27 15:30:32 +0300

    setup

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "users.apps.UserConfig",
-    'social_django',
+    "shop.apps.ShopConfig",
 ]
 
 MIDDLEWARE = [
